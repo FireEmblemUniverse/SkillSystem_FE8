71f452317305b1a95652f0a5b27be16f1984f606
Bovie Xiong <bovie.xiong@gmail.com>
2018-01-22 17:14:30 +0000

    minor changes

--- Engine Hacks/Skill System/Proc Skills/Chargeups.s
+++ Engine Hacks/Skill System/Proc Skills/Chargeups.s
@@ -19,6 +19,13 @@ cmp r5,#0x10
 ble DefCheck
 sub r5,#0x1
 strb r5,[r0,r4]
+mov r7, r0 @char data
+ldrb r0, [r7, #0xb] @char number
+ldr r6, =0x8019430
+mov lr, r6
+.short 0xf800
+strb r5, [r0, r4]
+mov r0, r7
 
 DefCheck:
 ldrb r5,[r1,r4]
@@ -26,6 +33,12 @@ cmp r5,#0x10
 ble End
 sub r5,#0x1
 strb r5,[r1,r4]
+mov r7, r1 @char data
+ldrb r0, [r7, #0xb] @char number
+ldr r6, =0x8019430
+mov lr, r6
+.short 0xf800
+strb r5, [r0, r4]
 
 End:
 pop {r4-r7}
