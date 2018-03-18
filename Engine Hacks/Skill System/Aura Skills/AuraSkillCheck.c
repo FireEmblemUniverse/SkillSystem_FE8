2620735370c988e56d38af6ca6065b64fbffede2
Stan H <deindall@gmail.com>
2018-03-18 12:32:27 +0100

    (new aura) Hacky "return buffer location in r1" fix

--- Engine Hacks/Skill System/Aura Skills/AuraSkillCheck.c
+++ Engine Hacks/Skill System/Aura Skills/AuraSkillCheck.c
@@ -11,7 +11,7 @@ static int absolute(int value) { return value < 0 ? -value : value; }
 
 static int AuraSkillTest(Unit* unit, Unit* other, int skill, int param);
 
-int AuraSkillCheck(Unit* unit, int skill, int param, int maxRange) {
+long long AuraSkillCheck(Unit* unit, int skill, int param, int maxRange) {
 	int count = 0;
 
 	for (int i = 0; i < 0x100; ++i) {
@@ -37,7 +37,19 @@ int AuraSkillCheck(Unit* unit, int skill, int param, int maxRange) {
 	}
 
 	gAuraUnitListOut[count] = 0;
-	return count;
+
+	union {
+		long long asLongLong;
+		struct {
+			int count;
+			uint8_t* pList;
+		};
+	} result;
+
+	result.count = count;
+	result.pList = gAuraUnitListOut;
+
+	return result.asLongLong;
 }
 
 int AuraSkillTest(Unit* unit, Unit* other, int skill, int param) {
