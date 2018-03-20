2f99ea8dd0a5ffc56d493874148c12591a4ab282
Stan H <deindall@gmail.com>
2018-03-20 19:14:44 +0100

    Working Armsthrift

--- Engine Hacks/Skill System/Other Skills/armsthrift.s
+++ Engine Hacks/Skill System/Other Skills/armsthrift.s
@@ -1,55 +1,80 @@
-@hook at 802c1f4
-
 .thumb
-.macro blh to, reg=r3
-  ldr \reg, =\to
-  mov lr, \reg
-  .short 0xf800
-.endm
-.equ ArmsthriftID, SkillTester+4
-.equ d100Result, 0x802a52c
-.equ luckgetter, 0x8019298
-@r4 is unit data
 
-@check last action was attack
-ldr r0, =0x203a958+0x11 @action taken
-ldrb r0, [r0]
-cmp r0, #2 @combat
-bne Normal
+GetItemAfterUse = 0x08016AEC+1
+RollBattleRN    = 0x0802A52C+1
+_ReturnLocation = 0x0802B828+1
 
-@check for armsthrift skill
-ldr r0, SkillTester
-mov lr, r0
-mov r0, r4 @attacker data
-ldr r1, ArmsthriftID
-.short 0xf800
-cmp r0, #0
-beq Normal
-@if user has Armsthrift, check for proc rate
-mov r0, r4
-blh luckgetter
-mov r1, r4 @skill user
-blh d100Result
-cmp r0, #1
-bne Normal
+LUnitHasSkill      = EALiterals+0x00
+LArmsthriftSkillID = EALiterals+0x04
 
-@if armsthrift and item is a weapon, do nothing
-pop {r4-r6}
-pop {r0}
-bx r0
+@ Hook from 0802B7F8
+ArmsthriftHook:
+	@ r0 is Current Round Data (first word)
+	@ r5 is Attacker
+	@ Nothing needs to be saved (we branch back to the function epilogue)
 
-Normal:
-ldrb r0, [r5, #8]
-strb r0, [r4, #8]
-ldrb r0, [r5, #9]
-strb r0, [r4, #9]
-ldrb r0, [r5, #0x13]
-strb r0, [r4, #0x13]
-ldr r0, =0x802c1ff
-bx r0
+	mov r1, #2   @ Miss flag
+
+	tst r0, r1  @ <void> = CurrentRound & 2
+	beq NonMiss @ goto NonMiss if zero (Miss flag is not set)
+
+	ldr r1, [r5, #0x4C]    @ BattleUnit.weaponAttributes
+	mov r2, #(0x02 | 0x80) @ IA_MAGIC | IA_UNCOUNTERABLE
+
+	tst r1, r2 @ <void> = BattleUnit.weaponAttributes & (IA_MAGIC | IA_UNCOUNTERABLE)
+	beq End    @ goto End if zero (weapon is neither magic or uncounterable)
+
+NonMiss:
+	@ ACTUAL ARMSTHRIFT CHECK BEGIN
+
+	mov r0, r5                 @ arg r0 = (Battle) Unit
+	ldr r1, LArmsthriftSkillID @ arg r1 = Skill Index
+
+	ldr r3, LUnitHasSkill
+	bl BXR3
+
+	cmp r0, #0        @ compare result
+	beq NonArmsthrift @ goto NonArmsthrift if zero (unit doesn't have armsthrift)
+
+	@ Getting Armsthrift proc chance (=luck)
+	ldrb r0, [r5, #0x19] @ BattleUnit.luck
+@	lsl  r0, #1          @ multiply by 2
+
+	@ ROLL
+	ldr r3, =RollBattleRN
+	bl BXR3
+
+	cmp r0, #0 @ compare result
+	bne End    @ goto End if non-zero (Armsthrift proc)
+
+NonArmsthrift:
+	@ ACTUAL ARMSTHRIFT CHECK END
+
+	mov  r4, #0x48 @ offsetof(BattleUnit.weaponAfter)
+
+	ldrh r0, [r5, r4] @ Load weapon
+
+	ldr r3, =GetItemAfterUse
+	bl BXR3
+
+	strh r0, [r5, r4] @ Store used weapon
+
+	cmp r0, #0 @ Compare weapon
+	bne End    @ goto End if weapon != 0
+
+	mov  r1, #0x7D @ BattleUnit.weaponBroke
+	mov  r0, #1
+
+	strb r0, [r5, r1] @ BattleUnit.weaponBroke = true
+
+End:
+	ldr r3, =_ReturnLocation
+BXR3:
+	bx  r3
 
 .ltorg
 .align
-SkillTester:
-@poin SkillTester
-@word AstraID
+
+EALiterals:
+	@ POIN SkillTester|1
+	@ WORD ArmsthriftID
