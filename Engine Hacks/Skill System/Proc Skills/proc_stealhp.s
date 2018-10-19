f468862c40a3198af0bcefea952a30ac2d835cb5
LeonarthCG <leonarth.cg@gmail.com>
2018-10-19 14:30:51 +0200

    More reversal fixes + reversal skills
    Made reversal and nosferatu play nice together
    Made it possible to set the skill bit on round data without having an animation (set animation to 0xFF, used for devil, bane and lethality)
    Added 3 skills related to devil reversal

--- Engine Hacks/Skill System/Proc Skills/proc_stealhp.s
+++ Engine Hacks/Skill System/Proc Skills/proc_stealhp.s
@@ -13,6 +13,15 @@ mov r5, r1 @defender
 mov r6, r2 @battle buffer
 mov r7, r3 @battle data
 
+@check for miss
+ldr     r0,[r2]           @r0 = battle buffer                @ 0802B40A 6800     
+lsl     r0,r0,#0xD                @ 0802B40C 0340     
+lsr     r0,r0,#0xD        @Without damage data                @ 0802B40E 0B40     
+mov	r1,#0x82 @miss + devil
+and	r0,r1
+cmp	r0,#2
+beq	End
+
 @check for draining weapon
 mov	r0,#0x4A
 ldrh	r0,[r4,r0]	@equipped item
@@ -24,16 +33,23 @@ add	r0,#31		@offset of weapon effect byte
 ldr	r1,=0x080177C0	@has table pointer
 ldr	r1,[r1]
 ldrb	r0,[r1,r0]	@weapon effect
-mov	r1,#2		@steal hp effect
-and	r0,r1
-cmp	r0,#0
-beq	End
+cmp	r0,#2		@steal hp effect
+bne	End
 
 @make sure damage > 0
 mov	r0,#4
 ldrsh	r0,[r7,r0]
 cmp	r0,#0
 ble	End
+
+@check for devil
+ldr     r0,[r2]           @r0 = battle buffer                @ 0802B40A 6800     
+lsl     r0,r0,#0xD                @ 0802B40C 0340     
+lsr     r0,r0,#0xD        @Without damage data                @ 0802B40E 0B40     
+mov	r1,#0x82 @miss + devil
+and	r0,r1
+cmp	r0,#0x80
+beq	noDamage
 
 @if we proc, set the hp update flag
 ldr     r2,[r6]    
@@ -96,6 +112,35 @@ strb	r2,[r6,#5]	@hp change
 End:
 pop {r4-r7}
 pop {r15}
+
+noDamage:
+@unset reversal
+ldr     r2,[r6]    
+lsl     r1,r2,#0xD                @ 0802B42C 0351     
+lsr     r1,r1,#0xD                @ 0802B42E 0B49     
+mov     r0,#0x80
+mvn	r0,r0
+and     r1,r0
+
+ldr     r0,=#0xFFF80000                @ 0802B434 4804     
+and     r0,r2                @ 0802B436 4010     
+orr     r0,r1                @ 0802B438 4308     
+str     r0,[r6]                @ 0802B43A 6018
+
+@mov	r0,#0x13
+@ldsb	r0,[r4,r0]	@remaining hp
+@mov	r2,#4
+@ldsb	r2,[r7,r2]	@damage
+@add	r0,r2
+@strb	r0,[r4,#0x13]	@remaining hp
+mov	r0,#0
+strb	r0,[r7,#4]
+strb	r0,[r7,#5]
+
+mov	r0,#0xFF	@no animation!
+strb	r0,[r6,#4]
+
+b	End
 
 .align
 .ltorg
