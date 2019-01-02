cb5fe7feeb857abfa46343700bcc38950188db50
Teraspark <33077030+Teraspark@users.noreply.github.com>
2019-01-01 22:00:40 -0500

    Added the Dead Unit Fix
    added the fix for dead units not being cleared

---
+++ Engine Hacks/Skill System/Teq Skills/FE8-Capture/FE8-Capture/Should_Dead_Unit_Be_Cleared.asm
@@ -0,0 +1,20 @@
+.thumb
+
+@r2 = char data of dead unit
+@return 1 in r1 if unit should be cleared, or 0 if they should not
+
+ldrb	r1,[r2,#0xB]
+mov		r0,#0xC0
+and		r1,r0
+cmp		r1,#0
+beq		GoBack		@player units don't get cleared
+ldr		r0,[r2,#0xC]
+mov		r3,#0x20	@being rescued
+tst		r0,r3
+beq		GoBack
+mov		r1,#0
+GoBack:
+ldr		r0,=#0x8018409
+bx		r0
+
+.ltorg
