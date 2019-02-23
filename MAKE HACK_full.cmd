af09b351bdf0c478ef9291961d2f7c617e5f8385
Stan H <deindall@gmail.com>
2019-02-23 12:56:57 +0100

    (move-tools) move tools to Tools folder, update windows make hacks

--- MAKE HACK_full.cmd
+++ MAKE HACK_full.cmd
@@ -1,21 +1,59 @@
-cd %~dp0
+@echo off
 
-copy FE8_clean.gba SkillsTest.gba
+@rem USAGE: "MAKE HACK_full.cmd" [quick]
+@rem If first argument is "quick", then this will not update text, tables, or generate a patch
+@rem "MACK HACK_quick.cmd" simply calls this but with the quick argument, for convenience
 
-cd "%~dp0Tables"
+@rem defining buildfile config
 
-echo: | (c2ea "%~dp0FE8_clean.gba")
+set "source_rom=%~dp0FE8_clean.gba"
 
-cd "%~dp0Text"
+set "main_event=%~dp0ROM Buildfile.event"
 
-echo: | (textprocess_v2 text_buildfile.txt)
+set "target_rom=%~dp0SkillsTest.gba"
+set "target_ups=%~dp0SkillsTest.ups"
+
+@rem defining tools
+
+set "c2ea=%~dp0Tools\C2EA\c2ea"
+set "texp=%~dp0Tools\TextProcess\textprocess_v2"
+set "ups=%~dp0Tools\ups\ups"
+
+@rem do the actual building
+
+copy "%source_rom%" "%target_rom%"
+
+if /I NOT "%1"==quick (
+
+  @rem only do the following if this isn't a make hack quick
+
+  echo "Processing tables"
+
+  cd "%~dp0Tables"
+  echo: | ("%c2ea%" "%source_rom%")
+
+  echo "Processing text"
+
+  cd "%~dp0Text"
+  echo: | ("%texp%" text_buildfile.txt)
+
+)
+
+echo "Assembling"
 
 cd "%~dp0Event Assembler"
+Core A FE8 "-output:%target_rom%" "-input:%~dp0ROM Buildfile.event"
 
-Core A FE8 "-output:%~dp0SkillsTest.gba" "-input:%~dp0ROM Buildfile.event"
+if /I NOT "%1"==quick (
 
-cd "%~dp0ups"
+  @rem only do the following if this isn't a make hack quick
 
-ups diff -b "%~dp0FE8_clean.gba" -m "%~dp0SkillsTest.gba" -o "%~dp0SkillsTest.ups"
+  echo "Generating patch"
 
+  cd "%~dp0"
+  %ups% diff -b "%source_rom%" -m "%target_rom%" -o "%target_ups%"
+
+)
+
+echo "Done!"
 pause
--- MAKE HACK_quick.cmd
+++ MAKE HACK_quick.cmd
@@ -1,9 +1,2 @@
-cd %~dp0
-
-copy FE8_clean.gba SkillsTest.gba
-
-cd "%~dp0Event Assembler"
-
-Core A FE8 "-output:%~dp0SkillsTest.gba" "-input:%~dp0ROM Buildfile.event"
-
-pause
+@echo off
+call "%~dp0MAKE HACK_full.cmd" quick
