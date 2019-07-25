03ac69d0fdabd7211e8925ece87a6988c7307d85
Snakey11 <38043506+Snakey11@users.noreply.github.com>
2019-07-25 19:06:15 -0400

    Move Frenzy to Pre-Battle Loop
    Frenzy conflicted with a hook for Str/Mag Split, and... I'm not really sure why it wasn't there in the first place. Maybe created before the look existed?

--- Engine Hacks/SkillSystem/Teq Skills/Frenzy.s
+++ Engine Hacks/SkillSystem/Teq Skills/Frenzy.s
@@ -1,25 +1,24 @@
-.thumb
+.thumb	@ Edited by Snek because of Str/Mag Split conflict. Moved to Pre-Battle Calc Loop.
 .org 0x0
 .equ FrenzyID, SkillTester+4
-push  {r14}
+push  {r4,r14} @ r0 = battle struct
 mov   r0,r4
-ldr   r1,SkillTester
-mov   r14,r1
 ldr   r1, FrenzyID
+ldr   r2,SkillTester
+mov   r14,r2
 .short  0xF800
 cmp   r0,#0x0
-beq   AddStr
+beq   NoAddAtk
 ldrb  r0,[r4,#0x12] @attacker max hp
 ldrb  r1,[r4,#0x13] @attacker current hp
 sub   r0,r1
 lsr   r0,#0x2     @missing hp/4
-add   r5,r0
-AddStr:
-add   r4,#0x5A
-mov   r0,#0x14
-ldsb  r0,[r6,r0]
-add   r5,r0
-strh  r5,[r4]
+mov   r2,#0x5A
+ldrh  r1,[r4,r2]
+add   r1,r0,r1
+strh  r1,[r4,r2]
+NoAddAtk:
+pop   {r4}
 pop   {r0}
 bx    r0
 
