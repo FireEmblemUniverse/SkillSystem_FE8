f4b13f2cb9d837d81ad79d1a6290a6087b1abfa7
Snakey11 <38043506+Snakey11@users.noreply.github.com>
2019-07-25 19:17:27 -0400

    Spirit Dust
    Spirit Dust stuff plus handling new Prep Screen item use screen.

--- Engine Hacks/Strmag/Str Mag Split/Prep Screen and Mag Booster/Prep Screen Usability1.asm
+++ Engine Hacks/Strmag/Str Mag Split/Prep Screen and Mag Booster/Prep Screen Usability1.asm
@@ -1,7 +1,7 @@
 .thumb
 .org 0x0
 
-add		r0,r1,r2
+add		r0,r0,r1
 strb	r0,[r6,#0x1D]
 mov		r1,r5
 add		r1,#0x3A
@@ -11,9 +11,9 @@ add		r1,r1,r2
 mov		r0,r6
 add		r0,#0x3A
 strb	r1,[r0]
+ldrb	r0,[r4,#0x08]
 ldrb	r1,[r5,#0x1A]
-ldrb	r4,[r4,#0x8]
-add		r0,r1,r4
+add		r0,r0,r1
 strb	r0,[r6,#0x1A]
 mov		r0,r6
 bx		r14
--- Engine Hacks/Strmag/Str Mag Split/Prep Screen and Mag Booster/Prep Screen Usability2.asm
+++ Engine Hacks/Strmag/Str Mag Split/Prep Screen and Mag Booster/Prep Screen Usability2.asm
@@ -1,21 +1,19 @@
 .thumb
 .org 0x0
 
-ldrb	r1,[r5,#0x1D]
-cmp		r2,r1
+ldsb	r0,[r5,r0]
+cmp		r0,r1
 beq		NoMovChange
-mov		r0,#0x1
+mov		r2,#0x1
 NoMovChange:
-mov		r1,r6
-add		r1,#0x3A
-ldrb	r1,[r1]
-mov		r2,r5
-add		r2,#0x3A
-ldrb	r2,[r2]
-cmp		r2,r1
+mov		r1,#0x3A
+ldsb	r1,[r6,r1]
+mov		r0,#0x3A
+ldsb	r0,[r5,r0]
+cmp		r0,r1
 beq		NoMagChange
-mov		r0,#0x1
+mov		r2,#0x1
 NoMagChange:
-ldrb	r6,[r6,#0x1A]
-ldrb	r5,[r5,#0x1A]
+mov		r1,#0x1A
+ldsb	r1,[r6,r1]
 bx		r14
