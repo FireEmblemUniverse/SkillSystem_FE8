6cc82d24dc79a196720ee02222ed9cd5c8e1c079
Sme <esahig15789@gmail.com>
2019-08-08 14:47:55 -0400

    5 drive skills

---
+++ Engine Hacks/SkillSystem/Aura Skills/DriveStr.s
@@ -0,0 +1,43 @@
+@Drive Def: adjacent allies gain +4 defense in combat.
+.equ DriveStrID, AuraSkillCheck+4
+.thumb
+push {r4-r7,lr}
+@goes in the battle loop.
+@r0 is the attacker
+@r1 is the defender
+mov r4, r0
+mov r5, r1
+
+mov r0, r4       @Move attacker data into r1.
+mov r1, #0x4c    @Move to the attacker's weapon ability
+ldr r1, [r0,r1]
+mov r2, #0x42
+tst r1, r2
+bne     Done @do nothing if magic bit set
+
+@now check for the skill
+ldr r0, AuraSkillCheck
+mov lr, r0
+mov r0, r4 @attacker
+ldr r1, DriveStrID
+mov r2, #0 @can_trade
+mov r3, #2 @range
+.short 0xf800
+cmp r0, #0
+beq Done
+
+mov r0, r4
+add     r0,#0x5A    @Move to the attacker's damage.
+ldrh    r3,[r0]     @Load the attacker's damage into r3.
+add     r3,#4    @add 4 to the attacker's damage.
+strh    r3,[r0]     @Store attacker avoid.
+
+Done:
+pop {r4-r7}
+pop {r0}
+bx r0
+.align
+.ltorg
+AuraSkillCheck:
+@ POIN AuraSkillCheck
+@ WORD DriveStrID
