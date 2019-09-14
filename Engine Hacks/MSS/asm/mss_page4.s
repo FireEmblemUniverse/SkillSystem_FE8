a6d4a29e4304380e8170526df95a68f48387dd82
Sme <esahig15789@gmail.com>
2019-09-13 22:49:19 -0400

    mss schenanigans part 1

--- Engine Hacks/MSS/asm/mss_page4.s
+++ Engine Hacks/MSS/asm/mss_page4.s
@@ -1,8 +1,25 @@
 .thumb
 .include "mss_defs.s"
+.set HolyBloodNameGetter, SS_BloodText+4
 
 page_start
 
-draw_textID_at 17, 9, textid=0xd4b, width=16, colour=Green
+draw_textID_at 17, 9, textID=0xd4b, width=16, colour=Green
+
+
+
+ldr r0, SS_BloodText
+draw_textID_at 17, 11 @Blood label text
+
+@pass in textid in r0
+blh HolyBloodNameGetter,r0
+draw_textID_at 17, 13, colour=White,
 
 page_end
+
+.align
+.ltorg
+.align
+SS_BloodText:
+@WORD SS_BloodText
+@POIN HolyBloodNameGetter
---
+++ Engine Hacks/SmeHax/HolyBlood/DescGetter.s
@@ -0,0 +1,38 @@
+.thumb
+.align 4
+
+
+.equ HolyBloodCharTable, HolyBloodTable+4
+
+
+push {r4-r7,r14}
+
+@return current character's holy blood desc in r0, or return 0xE0 (which is "--") if no blood
+@r8 = current unit data
+mov r0,r8
+ldr r0,[r0]
+ldr r0,[r0]
+ldrb r0,[r0,#4] @r0= char ID
+ldr r1,=HolyBloodCharTable
+add r1,r0
+ldrb r1,[r0] @r0 = holy blood ID
+cmp r0,#0xFF
+beq BadEnd
+ldr r1,=HolyBloodCharTable
+mov r2,#20
+mul r0,r2
+add r1,r0 @r1=table entry start, which is name ID
+add r1,#4 @r1=desc ID
+ldrh r0,[r1]
+b GoBack
+
+BadEnd:
+mov r0,#0xE0
+
+GoBack:
+pop {r4-r7}
+pop {r1}
+bx r1
+
+.ltorg
+HolyBloodTable:
