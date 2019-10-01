196173e1b5ba639c3ac6859d66139c1f15c98b9f
Snakey11 <38043506+Snakey11@users.noreply.github.com>
2019-10-01 17:14:23 -0400

    Update Faire skills to handle magic swords

--- Engine Hacks/SkillSystem/Ross Skills/FaireCheck.s
+++ Engine Hacks/SkillSystem/Ross Skills/FaireCheck.s
@@ -1,10 +1,29 @@
 .equ FaireIDList, SkillTester+4
 .thumb
 
+.macro blh to, reg
+    ldr \reg, =\to
+    mov lr, \reg
+    .short 0xF800
+.endm
+
 push {r4-r7, lr} 
-mov        r4,r0
-add     r0,#0x50    @Move to the attacking unit's weapon type.
-ldrb    r0,[r0]        @Load in the attacking unit's weapon type.    
+mov        r4,r0 @ r0 = battle struct.
+
+@ EDIT BY SNEK:
+	@ Getting the weapon type in this way doesn't account for magic swords.
+	@ I'm going to get the weapon type by item ID -> ROM item data -> weapon type.
+@add     r0,#0x50    @Move to the attacking unit's weapon type.
+@ldrb    r0,[r0]        @Load in the attacking unit's weapon type.    
+
+mov r1, #0x4A
+ldrh r0, [ r4, r1 ] @ Equipped item halfword.
+cmp r0, #0x00
+beq NoSkill
+blh 0x080174EC, r1 @ GetItemIndex. This function is such a meme tbh. r0 = item ID.
+blh 0x080177B0, r1 @ GetItemData. r0 = pointer to ROM item data.
+ldrb r0, [ r0, #0x07 ] @ r0 = this item's weapon type.
+
 ldr     r1,FaireIDList    @Load in the list of Faire Skills.
 ldrb     r1,[r1,r0]    @Load in the Faire Skill corresponding to the equipped weapon.
 mov     r0,r4        @Store attacker data into r0 (for the purposes of SkillTester).
