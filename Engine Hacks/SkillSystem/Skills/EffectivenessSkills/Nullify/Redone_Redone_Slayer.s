7fba3097217f119e64145fb99c39cda1d898d2e2
Sme <esahig15789@gmail.com>
2019-11-11 22:12:59 -0500

    Skybreaker
    For some reason teq's reworked slayer was checking the skill and then if you have the skill checking the slayer table for your class which is antithetical to the point of having it be a skill so we made it not do that here

---
+++ Engine Hacks/SkillSystem/Skills/EffectivenessSkills/Nullify/Redone_Redone_Slayer.s
@@ -0,0 +1,80 @@
+.thumb
+
+@jumped to from 16C88
+
+@r0=attacker battle struct, r1=defender battle struct
+
+@Slayer table outline: BYTE class_ID multiplier is_there_another_entry 0; SHORT class_types 0
+
+@tequila why the h*ck are you checking the slayer table after checking the skill making it still hardcoded to classes
+@this version just checks a given class type EA literal
+
+.equ SlayerID, SkillTester+4
+.equ NullifyID, SlayerID+4
+.equ SlayerClassType, NullifyID+4
+.equ SkybreakerID, SlayerClassType+4
+.equ SkybreakerClassType,SkybreakerID+4
+
+push	{r4-r6,r14}
+mov		r4,r0
+mov		r5,r1
+ldr		r0,[r5,#0x4]
+cmp		r0,#0
+beq		RetFalse
+
+Slayer:
+mov		r0,r4
+ldr		r1,SlayerID
+ldr		r3,SkillTester
+mov		r14,r3
+.short	0xF800
+cmp		r0,#0
+beq		Skybreaker
+
+ldr		r2,[r5,#4]
+mov		r1,#0x50
+ldrh	r2,[r2,r1]			@weaknesses defender unit has
+ldrh 	r0,SlayerClassType
+and		r0,r2
+cmp		r0,#0
+bne		NullifyCheck
+
+Skybreaker:
+mov		r0,r4
+ldr		r1,SkybreakerID
+ldr		r3,SkillTester
+mov		r14,r3
+.short	0xF800
+cmp		r0,#0
+beq		RetFalse
+
+ldr		r2,[r5,#4]
+mov		r1,#0x50
+ldrh	r2,[r2,r1]			@weaknesses defender unit has
+ldrh 	r0,SkybreakerClassType
+and		r0,r2
+cmp		r0,#0
+bne		NullifyCheck
+b		RetFalse
+
+NullifyCheck:
+mov		r0,r4
+ldr		r1,NullifyID
+ldr		r3,SkillTester
+mov		r14,r3
+.short	0xF800
+cmp		r0,#0
+bne		RetFalse
+mov		r0,#6
+b		GoBack
+RetFalse:
+mov		r0,#0
+GoBack:
+pop		{r4-r6}
+pop		{r1}
+bx		r1
+
+.ltorg
+.align
+SkillTester:
+@
