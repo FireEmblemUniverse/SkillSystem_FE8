86cab05a99edd5a7d33817cf5cd3cf4a049fcdcb
Sme <esahig15789@gmail.com>
2020-05-18 15:51:26 -0400

    combat arts

---
+++ Engine Hacks/SkillSystem/Skills/UnitMenuSkills/CombatArts/CombatArts.s
@@ -0,0 +1,54 @@
+.thumb
+.align
+
+
+.global CombatArtsUsability
+.type CombatArtsUsability, %function
+
+.global CombatArtsEffect
+.type CombatArtsEffect, %function
+
+
+
+
+CombatArtsUsability:
+push {r4-r7,r14}
+
+
+
+
+
+
+
+pop {r4-r7}
+pop {r1}
+bx r1
+
+.ltorg
+.align
+
+
+
+.equ StartMenuAdjusted,0x804EB98
+
+CombatArtsEffect:
+push {r14}
+
+@StartMenuAdjusted takes menu definition offset in r0
+
+ldr r0,=StartMenuAdjusted
+mov r14,r0
+ldr r0,=CombatArtsMenuDef
+.short 0xF800
+
+
+
+
+
+pop {r1}
+bx r1
+
+.ltorg
+.align
+
+
