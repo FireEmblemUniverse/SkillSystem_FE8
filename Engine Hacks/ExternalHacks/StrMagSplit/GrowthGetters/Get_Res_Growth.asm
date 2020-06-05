b74b4ed1949570aa7db4d50ea2d1ae3da08dc34a
Sme <esahig15789@gmail.com>
2020-06-05 14:53:19 -0400

    Char + Class Growths as config option

--- Engine Hacks/ExternalHacks/StrMagSplit/GrowthGetters/Get_Res_Growth.asm
+++ Engine Hacks/ExternalHacks/StrMagSplit/GrowthGetters/Get_Res_Growth.asm
@@ -1,10 +1,21 @@
 .thumb
 .org 0x0
 
+.equ ClassGrowthOption, Extra_Growth_Boosts+4
+
 @r0=battle struct or char data ptr
 ldr		r1,[r0]
 add		r1,#33
 ldrb	r1,[r1]		@res growth
+ldr 	r2,ClassGrowthOption
+cmp		r2,#0
+beq		GetExtraGrowthBoost
+ldr 	r2,[r0,#4]
+add		r2,#32
+ldrb	r2,[r2]
+add 	r1,r2
+
+GetExtraGrowthBoost:
 mov		r2,#15		@index of res boost
 ldr		r3,Extra_Growth_Boosts
 bx		r3
