4040ee77bd7a92a1bcad8c17659006a156420f94
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2021-04-06 12:51:58 -0400

    Patches

---
+++ Patches/256 colors Title background Installer/readme.txt
@@ -0,0 +1,8 @@
+instructions:
+don't use with the intro cinematic (the installer includes the skip)
+don't save your image with transparency just in case
+make your 256 colors .png image, with dimensions of 240x160
+make sure the first (the transparent one) color is black! it will be used for the fade
+name your image "titlebackgroundimage.png" and drop it in the same folder as the installer
+open the image in usenti and choose "image > export" and export the image with a .bin extension
+assemble!
---
+++ Patches/256 colors Title background Installer/titlebackgroundimage.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(titlebackgroundimage)
+
+//======================================================================
+//
+//	titlebackgroundimage, 240x160@8, 
+//	+ palette 256 entries, not compressed
+//	+ 600 tiles not compressed
+//	Total size: 512 + 38400 = 38912
+//
+//	Time-stamp: 2021-04-05, 16:06:44
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_TITLEBACKGROUNDIMAGE_H
+#define GRIT_TITLEBACKGROUNDIMAGE_H
+
+#define titlebackgroundimageTilesLen 38400
+extern const unsigned short titlebackgroundimageTiles[19200];
+
+#define titlebackgroundimagePalLen 512
+extern const unsigned short titlebackgroundimagePal[256];
+
+#endif // GRIT_TITLEBACKGROUNDIMAGE_H
+
+//}}BLOCK(titlebackgroundimage)
