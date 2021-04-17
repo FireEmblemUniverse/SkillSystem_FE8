1e9189b795e086f3cf33a3cf1eaee180e642e1b6
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2021-04-16 20:39:03 -0400

    Commit

---
+++ TextDump/AssembleText.cmd
@@ -0,0 +1,10 @@
+
+
+
+set "textprocess=%~dp0..\Tools\TextProcess\text-process-classic"
+set "parsefile=%~dp0..\EventAssembler\Tools\ParseFile.exe"
+
+echo: | ("%textprocess%" text_buildfile.txt --parser-exe "%parsefile%" --installer "InstallTextData.event" --definitions "TextDefinitions.event")
+
+
+pause
