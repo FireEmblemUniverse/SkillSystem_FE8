2b30462e9b63c20a9c16e85d3fb53fcefa8827b3
Teraspark <33077030+Teraspark@users.noreply.github.com>
2021-06-20 18:38:47 -0400

    Get Min & Max Item Range
    added routines to return the min range and max range after skills are taken into account

--- EngineHacks/Necessary/ItemRangeFix/ItemTargeting/ItemRangeGetterM.s
+++ EngineHacks/Necessary/ItemRangeFix/ItemTargeting/ItemRangeGetterM.s
@@ -1,6 +1,10 @@
 .thumb
 .global GetItemRangeM
 .type GetItemRangeM,%function
+.global GetItemRangeMin
+.type GetItemRangeMin,%function
+.global GetItemRangeMax
+.type GetItemRangeMax,%function
 
 @get item modified range (after checking stuff like skills, mag/2, etc.)
 @arguments:
@@ -11,16 +15,42 @@
 	@r1 = min range
 GetItemRangeM:
 push	{lr}
-@ldr 	r3, FillRange
-@bl Jump
 bl 	ItemRangeGetter
 lsr 	r1, r0, #0x10
 mov 	r2, #0xFF
 and 	r0, r2
 and 	r1, r2
 pop 	{r3}
-Jump:
 bx	r3
 .ltorg
 .align
-FillRange:
+
+@arguments:
+	@r0 = unit pointer
+	@r1 = item id
+@returns
+	@r0 = min range (after skill checks,etc.)
+GetItemRangeMin:
+push	{lr}
+bl 	ItemRangeGetter
+lsl r0, r0, #0x08
+lsr r0, r0, #0x18
+pop 	{r1}
+bx	r1
+.ltorg
+.align
+
+@arguments:
+	@r0 = unit pointer
+	@r1 = item id
+@returns
+	@r0 = max range (after skill checks,etc.)
+GetItemRangeMax:
+push	{lr}
+bl 	ItemRangeGetter
+lsl r0,r0,#0x18
+lsr r0,r0,#0x18
+pop 	{r1}
+bx	r1
+.ltorg
+.align
