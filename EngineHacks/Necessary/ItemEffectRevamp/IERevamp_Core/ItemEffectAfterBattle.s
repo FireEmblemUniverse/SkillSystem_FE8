7c613dede499b5bc65d704621bf25efed768267d
Teraspark <33077030+Teraspark@users.noreply.github.com>
2021-07-28 14:23:57 -0400

    AoE Status Staves
    replaced nightmare effect with an item effect for trying to apply a given status effect to all enemies in range (1rn roll against status staff accuracy)

--- EngineHacks/Necessary/ItemEffectRevamp/IERevamp_Core/ItemEffectAfterBattle.s
+++ EngineHacks/Necessary/ItemEffectRevamp/IERevamp_Core/ItemEffectAfterBattle.s
@@ -11,9 +11,10 @@
 	.short 0xF800
 .endm
 
-	.set gActionData,                       0x0203A958 		
+.set gActionData,                  0x0203A958
 
-	.set GetUnit,                           0x08019430
+.set GetUnit,                      0x08019430
+.set GetItemUseEffect,             0x0801773C
 
 .set EffectJumpTable, OffsetList + 0x0
 
@@ -28,6 +29,7 @@ ldrb r1, [r4,#0x12]
 lsl r1,r1,#0x1
 add r1, #0x1E
 ldrh r0, [r0,r1]
+_blh GetItemUseEffect
 mov r1, #0x10
 @get routine
 ldr r3, EffectJumpTable
