323a9e45175519a6742bd424c7cdc3d4095e74f3
Teraspark <33077030+Teraspark@users.noreply.github.com>
2021-08-25 13:31:00 -0400

    status 0xFF fix
    Fix the issue caused by #291 that caused status 0xFF to be applied to a target unit after using an item #295

--- EngineHacks/Necessary/ItemEffectRevamp/IERevamp_Core/ItemEffectAfterBattle.s
+++ EngineHacks/Necessary/ItemEffectRevamp/IERevamp_Core/ItemEffectAfterBattle.s
@@ -12,9 +12,15 @@
 .endm
 
 .set gActionData,                  0x0203A958
+.set gActiveBattleUnit,            0x0203A4EC @attacker
+.set gTargetBattleUnit,            0x0203A56C @defender
 
 .set GetUnit,                      0x08019430
 .set GetItemUseEffect,             0x0801773C
+
+@get the status effect from the target unit struct
+@and apply it to the actual unit
+.set ApplyItemStatustoTargetUnit,  0x0803001C
 
 .set EffectJumpTable, OffsetList + 0x0
 
@@ -30,16 +36,29 @@ lsl r1,r1,#0x1
 add r1, #0x1E
 ldrh r0, [r0,r1]
 _blh GetItemUseEffect
+cmp r0, #0x0
+beq targetunitstatus
 mov r1, #0x10
 @get routine
 ldr r3, EffectJumpTable
 _blr r3
 cmp r0, #0x0
-beq skip
+beq targetunitstatus
 @call routine
 mov r1,r0
 mov r0,r5
 _blr r1
+b skip
+
+targetunitstatus:
+ldr r0, =gTargetBattleUnit
+add r0, #0x6F
+ldrb r0,[r0]
+cmp r0, #0xFF
+beq skip
+cmp r0, #0x0
+beq skip
+_blh ApplyItemStatustoTargetUnit
 
 skip:
 pop {r4-r5}
