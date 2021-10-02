e64d3576943bad357963471c7f23cb27a7985038
Huichelaar <76848124+Huichelaar@users.noreply.github.com>
2021-10-02 11:56:12 +0200

    Changed assembly source extensions to from .asm to .s.
    Four assembly source files had their extensions changed from .asm to .s.
    
    Also changed the makefile for SkillProcDisplay to generate assembly in a .asm file instead of a .s file.

--- EngineHacks/SkillSystem/Internals/AssembleLynEvent.bat
+++ EngineHacks/SkillSystem/Internals/AssembleLynEvent.bat
@@ -7,10 +7,10 @@ SET LYN="C:\devkitPro\lyn.exe"
 @rem Assemble into an elf
 %as% -g -mcpu=arm7tdmi -mthumb-interwork %1 -o "%~n1.elf"
 
-if exist "Definitions.asm" (
+if exist "Definitions.s" (
 	
 	@rem Assemble definitions into a .elf if exists	
-	%as% -g -mcpu=arm7tdmi -mthumb-interwork "Definitions.asm" -o "Definitions.elf"
+	%as% -g -mcpu=arm7tdmi -mthumb-interwork "Definitions.s" -o "Definitions.elf"
 
 	@rem Assebmle into a .lyn.event with definitions
 	%LYN% "%~n1.elf" "Definitions.elf" > "%~n1.lyn.event"
