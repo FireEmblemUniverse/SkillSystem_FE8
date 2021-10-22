f7e175ca21b51cb582ad8747e6320cd6550cca3e
hypergammaspaces <HyperGammaSpaces@users.noreply.github.com>
2021-10-21 18:46:56 -0700

    add page2 comments, disassembly, and ballista hack

--- EngineHacks/Necessary/ModularStatScreen/pages/mss_defs.s
+++ EngineHacks/Necessary/ModularStatScreen/pages/mss_defs.s
@@ -2,7 +2,13 @@
 .equ MovGetter, 0x8019224 @defined in the modularstatgetter
 .equ DebuffTable, 0x203f100
 .equ BufferText, 0x800A240
+.equ GetStringFromIndex, 0x800A240
+.equ FilterSomeTextFromStandardBuffer, 0x0800A3B8
+.equ Text_AppendString, 0x08004004
+.equ Text_Draw, 0x08003E70
+.equ Text_GetColorID, 0x08003E64
 .equ DrawText, 0x800443C
+.equ DrawItemOnStatscreen, 0x08016a2c
 .equ DrawBar, 0x80870BC
 .equ DrawStat, 0x8004B94
 .equ DrawStatBonus, 0x08004BF0
@@ -10,6 +16,7 @@
 .equ TileBufferBase, 0x2003c2c
 .equ MagCheck, 0x8018A58
 .equ AidCheck, 0x80189B8
+.equ MaxHPGetter, 0x8019190	   
 .equ StrGetter, 0x80191b0
 .equ MagGetter, 0x80191b8
 .equ SklGetter, 0x80191d0
@@ -34,11 +41,15 @@
 .equ Const_200472C, 0x200472C
 .equ Const_2023D40, 0x2023D40
 .equ tile_origin, 0x2003c94
+.equ ItemStruct, 0x08809B10
 .equ Green, 4
 .equ Yellow, 3
 .equ Blue, 2
 .equ Grey, 1
 .equ White, 0
+
+.equ Deny_Statscreen_Class_Lo, 0x34
+.equ Deny_Statscreen_Class_Hi, 0x62
 
 .macro blh to, reg=r3
   push {\reg}
@@ -421,6 +432,35 @@
   blh DrawBar, r4
 .endm
 
+.macro draw_rating_at, tile_x, tile_y
+  push {r4}
+  mov r4, #0x0
+  mov r0, r8
+  blh MaxHPGetter
+  add r4, r4, r0
+  mov r0, r8
+  blh StrGetter
+  add r4, r4, r0
+  mov r0, r8
+  blh SklGetter
+  add r4, r4, r0
+  mov r0, r8
+  blh SpdGetter
+  add r4, r4, r0
+  mov r0, r8
+  blh LuckGetter
+  add r4, r4, r0
+  mov r0, r8
+  blh DefGetter
+  add r4, r4, r0
+  mov r0, r8
+  blh ResGetter
+  add r4, r4, r0
+  mov r0, r4
+  pop {r4}
+  mov r1, #Blue
+  draw_number_at \tile_x, \tile_y
+.endm
 .macro draw_con_number_at, tile_x, tile_y
   mov r1, r8
   ldr     r0,[r1,#0x4] @class
@@ -626,220 +666,304 @@
   blh     DrawIcon 
 .endm
 
-.macro draw_stats_box
-  ldr     r0,=#0x8A02204        @0808748A @buffer TSA
-  ldr     r4,=#0x2020188        @0808748C
-  mov     r1,r4       @0808748E
-  blh      #0x8012F50       @08087490
-  ldr     r0,=#0x20049EE        @08087494
-  mov     r2,#0xC1        @08087496
-  lsl     r2,r2,#0x6        @08087498
-  mov     r1,r4       @0808749A
-  blh      #0x80D74A0       @0808749C
-  ldr     r0,=#0x8205A24        @080874A0
-  blh      #0x8086E00        @080874A2
-  @numbers
-  ldr     r6,=StatScreenStruct        @08087532
-  ldr     r0,[r6,#0xC]        @08087534
-  blh      #0x8016B58       @08087536
-  mov     r4,r0       @0808753A
-  mov     r5,#0x0       @0808753C
-  ldr     r0,[r6,#0xC]        @0808753E
-  ldr     r0,[r0,#0x4]        @08087540
-  ldrb    r0,[r0,#0x4]        @08087542
-  cmp     r0,#0x62        @08087544
-  beq     loc_0x80875F8        @08087546
-  cmp     r0,#0x34        @08087548
-  beq     loc_0x808757C        @0808754A
-  cmp     r4,#0x0       @0808754C
-  blt     loc_0x808757C        @0808754E
-  lsl     r4,r4,#0x1        @08087550
-  add     r0,r4,#1       @08087552
-  lsl     r0,r0,#0x6        @08087554
-  ldr     r1,=#0x2003D4C        @08087556
-  add     r0,r0,r1        @08087558
-  mov     r1,#0x0       @0808755A
-  mov     r2,#0x35        @0808755C
-  blh      0x8004B0C        @0808755E
-  add     r0,r4,#2       @08087562
-  lsl     r0,r0,#0x6        @08087564
-  ldr     r1,=#0x200472E        @08087566
-  add     r0,r0,r1        @08087568
-  ldr     r1,=#0x8A02250        @0808756A
-  mov     r2,#0xC1        @0808756C
-  lsl     r2,r2,#0x6        @0808756E
-  blh      0x80D74A0        @08087570
-  ldr     r0,[r6,#0xC]        @08087574
-  add     r0,#0x1E        @08087576
-  add     r0,r0,r4        @08087578
-  ldrh    r5,[r0]       @0808757A
-  loc_0x808757C:
-  ldr     r0,=StatScreenStruct        @0808757C
-  ldr     r0,[r0,#0xC]        @0808757E
-  ldr     r0,[r0,#0x4]        @08087580
-  ldrb    r0,[r0,#0x4]        @08087582
-  cmp     r0,#0x62        @08087584
-  beq     loc_0x80875F8        @08087586
-  cmp     r0,#0x34        @08087588
-  beq     loc_0x80875F8        @0808758A
-  ldr     r4,=#0x200407C        @0808758C
-  ldr     r6,=#0x203A4EC        @0808758E
-  mov     r0,r6       @08087590
-  add     r0,#0x5A        @08087592
-  mov     r1,#0x0       @08087594
-  ldsh    r2,[r0,r1]        @08087596
-  mov     r0,r4       @08087598
-  mov     r1,#0x2       @0808759A
-  blh      #0x8004B94        @0808759C
-  mov     r0,r4       @080875A0
-  add     r0,#0x80        @080875A2
-  mov     r1,r6       @080875A4
-  add     r1,#0x60        @080875A6
-  mov     r3,#0x0       @080875A8
-  ldsh    r2,[r1,r3]        @080875AA
-  mov     r1,#0x2       @080875AC
-  blh      #0x8004B94        @080875AE
-  mov     r0,r4       @080875B2
-  add     r0,#0xE       @080875B4
-  mov     r1,r6       @080875B6
-  add     r1,#0x66        @080875B8
-  mov     r3,#0x0       @080875BA
-  ldsh    r2,[r1,r3]        @080875BC
-  mov     r1,#0x2       @080875BE
-  blh      #0x8004B94       @080875C0
-  add     r4,#0x8E        @080875C4
-  mov     r0,r6       @080875C6
-  add     r0,#0x62        @080875C8
-  mov     r6,#0x0       @080875CA
-  ldsh    r2,[r0,r6]        @080875CC
-  mov     r0,r4       @080875CE
-  mov     r1,#0x2       @080875D0
-  blh      #0x8004B94        @080875D2
-  b       loc_0x8087630        @080875D6
-  loc_0x80875F8:
-  ldr     r4,=#0x200407C        @080875F8
-  mov     r0,r4       @080875FA
-  mov     r1,#0x2       @080875FC
-  mov     r2,#0xFF        @080875FE
-  blh      #0x8004B94        @08087600
-  mov     r0,r4       @08087604
-  add     r0,#0x80        @08087606
-  mov     r1,#0x2       @08087608
-  mov     r2,#0xFF        @0808760A
-  blh      #0x8004B94        @0808760C
-  mov     r0,r4       @08087610
-  add     r0,#0xE       @08087612
-  mov     r1,#0x2       @08087614
-  mov     r2,#0xFF        @08087616
-  blh      #0x8004B94        @08087618
-  add     r4,#0x8E        @0808761C
-  ldr     r0,=#0x203A4EC        @0808761E
-  add     r0,#0x62        @08087620
-  mov     r1,#0x0       @08087622
-  ldsh    r2,[r0,r1]        @08087624
-  mov     r0,r4       @08087626
-  mov     r1,#0x2       @08087628
-  blh      #0x8004B94        @0808762A
-  mov     r5,#0x0       @0808762E
-  loc_0x8087630:
-  mov     r0,r5       @08087630
-  blh      #0x8016CC0        @08087632
-  mov     r5,r0       @08087636
-  ldr     r4,=#0x2003CB4        @08087638
-  blh      #0x8003EDC        @0808763A
-  mov     r1,#0x37        @0808763E
-  sub     r1,r1,r0        @08087640
-  mov     r0,r4       @08087642
-  mov     r2,#0x2       @08087644
-  mov     r3,r5       @08087646
-  blh      #0x8004480, r4        @08087648
-  mov     r4,#0x0       @0808764C
-  ldr     r0,=#0x2003D2C        @0808764E
-  ldr     r3,=#0x7060       @08087650
-  mov     r5,r3       @08087652
-  ldr     r6,=#0x2C2        @08087654
-  add     r2,r0,r6        @08087656
-  ldr     r1,=#0x7068       @08087658
-  mov     r3,r1       @0808765A
-  add     r6,#0x40        @0808765C
-  add     r1,r0,r6        @0808765E
+.macro draw_stats_box showBallista=0
+  ldr     r0, =#0x8A02204 	@box TSA
+  ldr     r4, =#0x2020188 	@gGenericBuffer
+  mov     r1, r4
+  blh     #0x8012F50 		@Decompress
+  ldr     r0, =#0x20049EE 	@somewhere on the bgmap
+  mov     r2, #0xC1
+  lsl     r2, r2, #0x6
+  mov     r1, r4
+  blh     #0x80D74A0 		@BgMap_ApplyTsa
+  ldr     r0, =#0x8205A24 	@map of text labels and positions
+  blh     #0x8086E00        @DrawStatscreenTextMap
+  ldr     r6, =StatScreenStruct
+  ldr     r0, [r6, #0xC]
+  ldr     r0, [r0, #0x4]
+  ldrb    r0, [r0, #0x4]
+  cmp     r0, #Deny_Statscreen_Class_Hi
+    beq     SS_DoneEquipHighlightBar
+  cmp     r0, #Deny_Statscreen_Class_Lo
+    beq     SS_DoneEquipHighlightBar
+  
+    .if \showBallista
+
+		ldr  r2, [r6, #0xC]
+		ldr  r0, [r2, #0xC]
+		mov  r1, #0x80        
+		lsl  r1, r1, #0x4		@Check "in ballista" bit
+		and  r0, r1
+		cmp  r0, #0x0
+		beq  NoBallistaEquipped_Box
+		
+		@get a non-empty ballista at unit position
+		mov  r0, #0x10
+		mov  r1, #0x11
+		ldsb r0, [r2, r0]
+		ldsb r1, [r2, r1]
+		blh  #0x803798C 		@GetBallistaItemAt
+		cmp  r0, #0x0
+		beq  NoBallistaEquipped_Box
+		mov  r5, r0
+		mov  r4, #0x0 			@slot id
+		b    SS_DrawEquippedItemHighlight
+		
+	.endif
+  
+  NoBallistaEquipped_Box:
+  ldr     r0, [r6, #0xC] 
+  blh     #0x8016B58 		@GetUnitEquippedItemSlot
+  mov     r4, r0
+  mov     r5, #0x0
+  cmp     r4, #0x0 			@no equipped item will be -1
+  blt     SS_DoneEquipHighlightBar
+  
+  SS_DrawEquippedItemHighlight:
+  lsl     r4, r4, #0x1
+  add     r0, r4, #1
+  lsl     r0, r0, #0x6
+  ldr     r1, =#0x2003D4C
+  add     r0, r0, r1
+  mov     r1, #0x0
+  mov     r2, #0x35			@the equip 'E'
+  blh     #0x8004B0C 		@DrawSpecialUiChar
+  add     r0, r4, #2
+  lsl     r0, r0, #0x6
+  ldr     r1, =#0x200472E
+  add     r0, r0, r1
+  ldr     r1, =#0x8A02250 	@TSA for highlight bar
+  mov     r2, #0xC1
+  lsl     r2, r2, #0x6
+  blh     #0x80D74A0 		@BgMap_ApplyTsa
+  
+  cmp     r5, #0x0
+  bne     SS_DoneEquipHighlightBar
+  
+  SS_ItemBox_GetID:
+  ldr     r0, [r6, #0xC]
+  add     r0, #0x1E
+  add     r0, r0, r4
+  ldrh    r5, [r0]
+  
+  SS_DoneEquipHighlightBar:
+  ldr     r0, =StatScreenStruct
+  ldr     r0, [r0, #0xC]
+  ldr     r0, [r0, #0x4]
+  ldrb    r0, [r0, #0x4]
+  cmp     r0, #Deny_Statscreen_Class_Hi
+  beq     SS_DrawItemBox_Unarmed
+  cmp     r0, #Deny_Statscreen_Class_Lo
+  beq     SS_DrawItemBox_Unarmed
+  
+  ldr     r4, =#0x200407C 	@bgmap offset
+  ldr     r6, =#0x203A4EC 	@gActiveBattleUnit
+  mov     r0, r6
+  add     r0, #0x5A 		@load battle atk
+  mov     r1, #0x0
+  ldsh    r2, [r0, r1]
+  mov     r0, r4
+  mov     r1, #0x2
+  blh     #0x8004B94 		@DrawUiNumber
+  mov     r0, r4
+  add     r0, #0x80
+  mov     r1, r6
+  add     r1, #0x60 		@load battle hit
+  mov     r3, #0x0
+  ldsh    r2, [r1, r3]
+  mov     r1, #0x2
+  blh     #0x8004B94 		@DrawUiNumber
+  mov     r0, r4
+  add     r0, #0xE
+  mov     r1, r6
+  add     r1, #0x66 		@load battle crit
+  mov     r3, #0x0
+  ldsh    r2, [r1, r3]
+  mov     r1, #0x2
+  blh     #0x8004B94 		@DrawUiNumber
+  add     r4, #0x8E
+  mov     r0, r6
+  add     r0, #0x62 		@load battle avoid
+  mov     r6, #0x0
+  ldsh    r2, [r0, r6]
+  mov     r0, r4
+  mov     r1, #0x2
+  blh     #0x8004B94 		@DrawUiNumber
+  b       SS_DrawItemBox_RangeText
+  
+  SS_DrawItemBox_Unarmed:
+  ldr     r4, =#0x200407C
+  mov     r0, r4
+  mov     r1, #0x2
+  mov     r2, #0xFF
+  blh     #0x8004B94 		@DrawUiNumber
+  mov     r0, r4
+  add     r0, #0x80
+  mov     r1, #0x2
+  mov     r2, #0xFF
+  blh     #0x8004B94 		@DrawUiNumber
+  mov     r0, r4
+  add     r0, #0xE
+  mov     r1, #0x2
+  mov     r2, #0xFF
+  blh     #0x8004B94 		@DrawUiNumber
+  add     r4, #0x8E
+  ldr     r0, =#0x203A4EC 	@gActiveBattleUnit
+  add     r0, #0x62 		@load battle avoid
+  mov     r1, #0x0
+  ldsh    r2, [r0, r1]
+  mov     r0, r4
+  mov     r1, #0x2
+  blh     #0x8004B94 		@DrawUiNumber
+  mov     r5, #0x0			@set item as blank
+  
+  SS_DrawItemBox_RangeText:
+  mov     r0, r5
+  blh     #0x8016CC0 		@GetItemRangeString
+  mov     r5, r0
+  ldr     r4, =#0x2003CB4
+  blh     #0x8003EDC 		@Text_GetStringTextWidth
+  mov     r1, #0x37
+  sub     r1, r1, r0
+  mov     r0, r4
+  mov     r2, #0x2
+  mov     r3, r5
+  blh     #0x8004480, r4 	@Text_InsertString
+  mov     r4, #0x0
+  ldr     r0, =#0x2003D2C
+  ldr     r3, =#0x7060
+  mov     r5, r3
+  ldr     r6, =#0x2C2
+  add     r2, r0, r6
+  ldr     r1, =#0x7068
+  mov     r3, r1
+  add     r6, #0x40
+  add     r1, r0, r6
+  
+  @i think this loop just clears a gfx buffer
   loc_0x8087660:
-  add     r0,r4,r5        @08087660
-  strh    r0,[r2]       @08087662
-  add     r0,r4,r3        @08087664
-  strh    r0,[r1]       @08087666
-  add     r2,#0x2       @08087668
-  add     r1,#0x2       @0808766A
-  add     r4,#0x1       @0808766C
-  cmp     r4,#0x7       @0808766E
-  ble     loc_0x8087660        @08087670
+  add     r0, r4, r5
+  strh    r0, [r2]
+  add     r0, r4, r3
+  strh    r0, [r1]
+  add     r2, #0x2
+  add     r1, #0x2
+  add     r4, #0x1
+  cmp     r4, #0x7
+  ble     loc_0x8087660
+  
 .endm
 
-.macro draw_items_text
-  push {r7}
-  mov r7, r8
-  push {r7}
-  ldr     r2,=StatScreenStruct        @080874A6
-  ldr     r1,[r2,#0xC]        @080874A8
-  ldr     r0,[r1,#0x4]        @080874AA
-  ldrb    r0,[r0,#0x4]        @080874AC
-  cmp     r0,#0x62        @080874AE
-  beq     loc_0x8087532       @080874B0
-  cmp     r0,#0x34        @080874B2
-  beq     loc_0x8087532       @080874B4
-  mov     r4,#0x0       @080874B6
-  ldrh    r5,[r1,#0x1E]       @080874B8
-  cmp     r5,#0x0       @080874BA
-  beq     loc_0x8087532       @080874BC
-  mov     r7,r2       @080874BE
-  mov     r8,r4       @080874C0
-  mov     r6,#0x40        @080874C2
-  loc_0x80874C4:
-  ldr     r2,[r7,#0xC]        @080874C4
-  ldr     r0,[r2,#0xC]        @080874C6
-  mov     r1,#0x80        @080874C8
-  lsl     r1,r1,#0x5        @080874CA
-  and     r0,r1       @080874CC
-  cmp     r0,#0x0       @080874CE
-  beq     loc_0x80874F8       @080874D0
-  mov     r0,r2       @080874D2
-  blh      #0x80179D8       @080874D4
-  sub     r0,#0x1       @080874D8
-  cmp     r4,r0       @080874DA
-  bne     loc_0x80874F8       @080874DC
-  mov     r2,#0x4       @080874DE
-  b       loc_0x808750A       @080874E0
+.macro draw_items_text showBallista=0
+  push    {r7}
+  mov     r7, r8
+  push    {r7}
+  ldr     r2, =StatScreenStruct
+  ldr     r1, [r2, #0xC]
+  ldr     r0, [r1, #0x4]
+  ldrb    r0, [r0, #0x4]
+  cmp     r0, #Deny_Statscreen_Class_Hi
+  beq     GorgonEggSkip_ItemList
+  cmp     r0, #Deny_Statscreen_Class_Lo
+  bne     SS_StartItemsList
+  
+  GorgonEggSkip_ItemList:
+  b       SS_FinishItemsList
+  
+  SS_StartItemsList:
+  mov     r6, #0x40
+  mov     r7, r2
+  
+  .if \showBallista
+
+		ldr  r2, [r7, #0xC]
+		ldr  r0, [r2, #0xC]
+		mov  r1, #0x80        
+		lsl  r1, r1, #0x4		@Check "in ballista" bit
+		and  r0, r1
+		cmp  r0, #0x0
+		beq  NoBallistaEquipped
+		
+		@get a non-empty ballista at unit position
+		mov  r0, #0x10
+		mov  r1, #0x11
+		ldsb r0, [r2, r0]
+		ldsb r1, [r2, r1]
+		blh  #0x803798C 		@GetBallistaItemAt
+		cmp  r0, #0x0
+		beq  NoBallistaEquipped
+		mov  r5, r0
+		
+		mov  r4, #0x2
+		neg  r4, r4
+		mov  r8, r4
+		mov  r4, #0x0 			@draws item 1 in slot 2
+		mov  r2, #Yellow
+		b    SS_DrawItemName
+
+  .endif  
+  
+  NoBallistaEquipped:
+  ldr     r1, [r7, #0xC]
+  mov     r4, #0x0
+  ldrh    r5, [r1, #0x1E]
+  cmp     r5, #0x0			@does unit have items left?
+  beq     SS_FinishItemsList
+  mov     r8, r4
+  
+  SS_LoopItemsList:
+  ldr     r2, [r7, #0xC]
+  ldr     r0, [r2, #0xC]
+  mov     r1, #0x80        
+  lsl     r1, r1, #0x5		@Check "drop item" bit
+  and     r0, r1
+  cmp     r0, #0x0
+  beq     SS_NotDroppableItem
+  
+  mov     r0, r2
+  blh     #0x80179D8		@GetUnitItemCount
+  sub     r0, #0x1
+  cmp     r4, r0
+  bne     SS_NotDroppableItem
+  mov     r2, #Green
+  b       SS_DrawItemName
+  
   .ltorg
-  loc_0x80874F8:
-  ldr     r0,[r7,#0xC]        @080874F8
-  mov     r1,r5       @080874FA
-  blh      #0x8016EE4       @080874FC
-  mov     r2,#0x0       @08087500
-  lsl     r0,r0,#0x18       @08087502
-  cmp     r0,#0x0       @08087504
-  bne     loc_0x808750A       @08087506
-  mov     r2,#0x1       @08087508
-  loc_0x808750A:
-  lsl     r0,r4,#0x3        @0808750A
-  ldr     r1,=#0x2003C8C        @0808750C
-  add     r0,r0,r1        @0808750E
-  ldr     r3,=#0x2003D2E        @08087510
-  add     r3,r6,r3        @08087512
-  mov     r1,r5       @08087514
-  blh      #0x8016A2C, r5       @08087516
-  mov     r0,#0x2       @0808751A
-  add     r8,r0       @0808751C
-  add     r6,#0x80        @0808751E
-  add     r4,#0x1       @08087520
-  cmp     r4,#0x4       @08087522
-  bgt     loc_0x8087532       @08087524
-  ldr     r0,[r7,#0xC]        @08087526
-  add     r0,#0x1E        @08087528
-  add     r0,r8       @0808752A
-  ldrh    r5,[r0]       @0808752C
-  cmp     r5,#0x0       @0808752E
-  bne     loc_0x80874C4       @08087530
-  loc_0x8087532:
+  
+  SS_NotDroppableItem:
+  ldr     r0, [r7, #0xC]
+  mov     r1, r5
+  blh     #0x8016EE4		@IsItemUsable
+  mov     r2, #White
+  lsl     r0, r0, #0x18
+  cmp     r0, #0x0
+  bne     SS_DrawItemName
+    mov     r2, #Grey
+  SS_DrawItemName:
+  lsl     r0, r4, #0x3
+  ldr     r1, =#0x2003C8C
+  add     r0, r0, r1
+  ldr     r3, =#0x2003D2E	@ypos?
+  add     r3, r6, r3
+  mov     r1, r5
+  blh     #0x8016A2C, r5	@DrawItemOnStatscreen
+  mov     r0, #0x2
+  add     r8, r0
+  add     r6, #0x80
+  add     r4, #0x1
+  cmp     r4, #0x4
+  bgt     SS_FinishItemsList
+  ldr     r0, [r7, #0xC]
+  add     r0, #0x1E
+  add     r0, r8
+  ldrh    r5, [r0]
+  cmp     r5, #0x0
+  bne     SS_LoopItemsList
+  b		  SS_FinishItemsList
+  
+  .ltorg
+  
+  SS_FinishItemsList:
   pop {r7}
   mov r8, r7
   pop {r7}
