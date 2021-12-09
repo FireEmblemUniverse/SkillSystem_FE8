30f41c72dea6801cbc8d33cd1ec8fca8b693b701
Sme <esahig15789@gmail.com>
2021-12-09 04:14:05 -0500

    Alter WaitAction
    Previously, this function would set flag 0x40 in the unit state bitfield and nothing else. This is unnecessary and breaks canto under specific circumstances.

--- EngineHacks/Necessary/UnitActionRework/asm/WaitAction.s
+++ EngineHacks/Necessary/UnitActionRework/asm/WaitAction.s
@@ -1,15 +1,16 @@
 .thumb
-.include "_Definitions.h.s"
+//.include "_Definitions.h.s"
 
 WaitAction:
-	ldr r0, =ppActiveUnit
-	ldr r0, [r0]
+//	Unnecessary and breaks canto in some circumstances
+//	ldr r0, =ppActiveUnit
+//	ldr r0, [r0]
 	
-	ldr r1, [r0, #0x0C] @ Loading State
-		mov r2, #0x40 @ Has Already Moved Flag
-		orr r1, r2
-	str r1, [r0, #0x0C] @ Storing State
-	
+//	ldr r1, [r0, #0x0C] @ Loading State
+//		mov r2, #0x40 @ Has Already Moved Flag
+//		orr r1, r2
+//	str r1, [r0, #0x0C] @ Storing State
+
 	mov r0, #1 @ Continue (6C)
 	
 	bx lr
