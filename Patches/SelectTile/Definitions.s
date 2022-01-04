138287a3c0e7b2827adabef276abbdb8c324b23c
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2022-01-03 19:56:22 -0500

    Commit

--- Patches/SelectTile/Definitions.s
+++ Patches/SelectTile/Definitions.s
@@ -25,15 +25,26 @@ SET_FUNC RTextLeft, (0x080893B4+1)
 
 SET_FUNC RTextRight, (0x080893E4+1)
 
+SET_FUNC GetUnitEquippedItem, (0x08016B28+1)
 
+SET_FUNC StartMovingPlatform, (0x080CD408+1)
 
+SET_FUNC SetupMovingPlatform, (0x080CD47C+1)
+SET_FUNC DeleteSomeAISStuff, (0x0805AA28+1)
+
+SET_FUNC DeleteSomeAISProcs, (0x0805AE14+1)
 
 //@ Data declarations:
 SET_DATA gBG0MapBuffer, 0x02022CA8
 
+SET_DATA gBG1MapBuffer, 0x020234A8
+
 SET_DATA gRAMMenuCommands, 0x203F548
 //0x203F382 // allocated 0x230 bytes for pokemblem ram and 0x80 bytes for this 
 
+SET_DATA gSomeAISStruct, 0x030053A0
+
+SET_DATA gSomeAISRelatedStruct, 0x0201FADC
 
 SET_DATA MemorySlot, 0x30004B8
 
