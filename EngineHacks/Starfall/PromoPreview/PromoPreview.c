dc2da4cc20e36df1f29355addab42656d11d3f1d
Sme <esahig15789@gmail.com>
2022-02-06 15:40:24 -0500

    Update PromoPreview.c

--- EngineHacks/Starfall/PromoPreview/PromoPreview.c
+++ EngineHacks/Starfall/PromoPreview/PromoPreview.c
@@ -20,6 +20,33 @@ u8 CheckPromoItem(u16 item) {
 void DrawPromoPreview(u16 item) {
 
 	//need to get the class we would promote into with the promo item, if any
+	item = (item & 0xFF); //just item ID
+	struct PromoItemsListEntry* promoList = &PromoItemsList;
+	while (promoList->itemID != 0) {
+	
+		if (item == promoList->itemID) break;
+		promoList ++;
 		
+	}
+	if (promoList->itemID == 0) return;
+	
+	struct Unit activeUnit = *gActiveUnit;
+	
+	struct PromoItemClassListEntry* promoClassList = promoList->promoList;
+	while (promoClassList->baseClass != 0) {
+		
+		if (activeUnit.pClassData->number == promoClassList->baseClass) break;
+		promoClassList ++;
+	}
+	if (promoClassList->baseClass == 0) return;
+	u8 promotedClass = promoClassList->promoClass;
+	ClassData promotedClassEntry = gClassData[promotedClass];
+	
+	//draw fixed (externalized) text ID @ ~(132,98)
+	//draw class name @ ~(132,112)
+	//draw icons for weapons w/o 0 base rank  
+	//on promoted class @ ~(132,128), padded 
+	//draw class type icon @ ~(208,104)
+	//draw class skill icon @ ~(208,128)
 	
 }
