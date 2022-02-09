61dbfdca7d8f1c662bccb90bf6f5e5fa15439194
Sme <esahig15789@gmail.com>
2022-02-08 21:33:00 -0500

    gaming!

--- EngineHacks/Starfall/PromoPreview/PromoPreview.c
+++ EngineHacks/Starfall/PromoPreview/PromoPreview.c
@@ -43,10 +43,57 @@ void DrawPromoPreview(u16 item) {
 	ClassData promotedClassEntry = gClassData[promotedClass];
 	
 	//draw fixed (externalized) text ID @ ~(136,96)
-	//draw class name @ ~(136,112)
-	//draw icons for weapons w/o 0 base rank  
-	//on promoted class @ ~(136,128), padded 
-	//draw class type icon @ ~(208,104)
-	//draw class skill icon @ ~(208,128)
+	const char* textID = GetStringFromIndex(PromotesToTextIDLink);
+	TextHandle handle = {
+			.tileIndexOffset = gpCurrentFont->tileNext,
+			.tileWidth = 7
+	};
+	Text_Clear(&handle);
+	Text_DrawString(&handle, textID);
+	Text_Display(&handle, &gBG0MapBuffer[12][16]);
 	
+	//draw class name @ ~(14,16)
+	const char* classTextID = GetStringFromIndex(promotedClassEntry.nameTextId);
+	
+	TextHandle classHandle = {
+		.tileIndexOffset = gpCurrentFont->tileNext+7,
+		.tileWidth = 12
+	};
+	Text_Clear(&classHandle);
+	Text_DrawString(&classHandle,classTextID);
+	Text_Display(&classHandle, &gBG0MapBuffer[14][16]);
+	
+
+	LoadIconPalette(1, 0xD);
+	LoadIconPalette(0, 0xE);
+
+	//draw class type icon @ ~(26,14)
+	int classIcon = 0xFF;
+	int attributes = promotedClassEntry.attributes;
+	if ((attributes & CA_MOUNTED) != 0) classIcon = 0;
+		if ((attributes & CA_PEGASUS) != 0) classIcon = 1;
+		
+		if ((attributes & CA_WYVERN) != 0) classIcon = 2;
+	
+	if (classIcon != 0xFF) {
+		DrawIcon(&gBG0MapBuffer[14][26],classIcon|0x300,0xD000);
+	}
+
+	//draw class skill icon @ ~(26,16)
+	if (ClassSkillTable[promotedClass] != 0) {
+		DrawIcon(&gBG0MapBuffer[16][26],ClassSkillTable[promotedClass]|0x100,0xE000);
+	}
+
+	//draw icons for weapons w/o 0 base rank  
+	//on promoted class @ ~(16,16), padded 
+	//not padded actually because lol we have room for like 3 if we pad
+	int wtIconOffset = 16;
+	int wtIconIndex = 0;
+	while (wtIconIndex < 8) {
+		if (promotedClassEntry.baseRanks[wtIconIndex] != 0) {
+			DrawIcon(&gBG0MapBuffer[16][wtIconOffset],wtIconIndex|0x400,0xD000);
+			wtIconOffset += 2;
+		}
+		wtIconIndex ++;
+	}
 }
--- EngineHacks/Starfall/PromoPreview/PromoPreview.h
+++ EngineHacks/Starfall/PromoPreview/PromoPreview.h
@@ -4,6 +4,9 @@
 u8 CheckPromoItem(u16 item);
 void DrawPromoPreview(u16 item);
 
+extern u16 gBG0MapBuffer[32][32]; // 0x02022CA8. Ew why does FE-CLib-master not do it like this?
+extern u16 gBG1MapBuffer[32][32]; // 0x020234A8.
+extern u16 gBG2MapBuffer[32][32]; // 0x02023CA8.
 
 struct PromoItemsListEntry
 {
@@ -19,3 +22,6 @@ struct PromoItemClassListEntry
 
 extern struct PromoItemsListEntry PromoItemsList;
 
+extern int PromotesToTextIDLink;
+
+extern u8 ClassSkillTable[256];
