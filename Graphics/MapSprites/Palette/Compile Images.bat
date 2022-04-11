7e1ce8db7307761aa187a2414eaad1fdfa29d597
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2022-04-11 12:52:58 -0400

    Commit

--- Graphics/MapSprites/Palette/Compile Images.bat
+++ Graphics/MapSprites/Palette/Compile Images.bat
@@ -1,11 +1,10 @@
 @echo off
 
-@set "Png2Dmp=%~dp0/../../EventAssembler/Tools/Png2Dmp.exe"
-@set "Png2Dmp=%~dp0/Png2Dmp.exe"
+@set "Png2Dmp=%~dp0/../../../EventAssembler/Tools/Png2Dmp.exe"
 
 @dir *.png /b > png.txt
 
-@for /f "tokens=*" %%m in (png.txt) do ("%Png2Dmp%" "%%~m" --lz77 -po "%%~nm_pal.dmp")
+@for /f "tokens=*" %%m in (png.txt) do ("%Png2Dmp%" "%%~m" -po "%%~nm_pal.dmp")
 
 @del png.txt
 
