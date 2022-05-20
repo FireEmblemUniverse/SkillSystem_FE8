e280333319c602ea0a2088dd32fad66fea07579e
masterofcontroversy <masterofcontroversy@mail.com>
2022-05-20 06:55:50 -0400

    Add faster PreBattleCalcLoop, Turn AuraSkill list to AuraSkillTable

--- EngineHacks/Necessary/CalcLoops/PreBattleCalcLoop/FE8_battle_calc_loop.s
+++ EngineHacks/Necessary/CalcLoops/PreBattleCalcLoop/FE8_battle_calc_loop.s
@@ -1,53 +1,32 @@
-.org 0
 .thumb
 
-LoopParent:
-push {lr}
-mov r2,pc
-add r2, #(LoopTable - Subtract) @need to double check here
-Subtract:
-bl MainLoop
-pop {r0}
-bx r0
-
-MainLoop:
-push {r4-r6,lr}
-mov r4,r0
-mov r5,r1
-mov r6,r2
 StartLoop:
-ldr r2,[r6]
-cmp r2,#0 @end of loop?
-beq EndLoop
-mov r0,#1
-orr r2,r0 @in case you forgot the +1... get rid of this if you want ARM code
-mov r0,r4
-mov r1,r5
-bl RunCalc
-add r6,#4
-b StartLoop
-EndLoop:
+    push {r4-r6, lr}
+    mov  r4, r0
+    mov  r5, r1
 
-pop {r4-r6}
-pop {r0}
-bx r0
+    mov  r6, #0x0 @initialize for loop
+    Loop:
+        ldr  r2, PreBattlePointers
+        ldr  r2, [r2, r6]
+        cmp  r2, #0x0
+        beq  EndLoop
+            mov  r0, #0x1
+            orr  r2, r0 @Make sure we're in thumb mode
+            mov  r0, r4
+            mov  r1, r5
+            mov  lr, r2
+            .short 0xF800
+            add  r6, #0x4
+            b    Loop
 
-RunCalc:
-push {r4-r6,lr}
-mov r4,r0
-mov r5,r1
-mov r6,r2
-bl GOTO_r6
-@ mov r0,r5
-@ mov r1,r4
-@ bl GOTO_r6 @don't flip it???
-pop {r4-r6}
-pop {r0}
-bx r0
-GOTO_r6:
-bx r6
+    EndLoop:
+    pop  {r4-r6}
+    pop  {r0}
+    bx   r0
 
-.align 2
+.align
 .ltorg
-LoopTable:
-@this is a table of pointers
+PreBattlePointers:
+@POIN PreBattleFunctions
+@it should contain a list of pointers to functions terminated by a four bytes of 0
