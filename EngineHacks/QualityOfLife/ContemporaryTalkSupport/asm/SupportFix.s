f802ba9cd04416b7ea887b9f4dbf58a349302550
Sme <esahig15789@gmail.com>
2022-07-15 20:07:41 -0400

    update supportfix (#397)

--- EngineHacks/QualityOfLife/ContemporaryTalkSupport/asm/SupportFix.s
+++ EngineHacks/QualityOfLife/ContemporaryTalkSupport/asm/SupportFix.s
@@ -3,12 +3,12 @@
 .thumb
 .org 0
 
-push {r4-r5,lr}
+push {r4-r5}
 
-ldr r4,[sp,#0x18]   @=0x203a958
+ldr r4,=0x203a958
 mov r5,#0x17        @(bought from armory - guaranteed safe!)
 strb r5,[r4,#0x11]
-ldr r4,[sp,#0x14]   @pointer to "next routine" - we change this to take us back into the unit menu
+ldr r4,[sp,#0x10]   @pointer to "next routine" - we change this to take us back into the unit menu
 ldr r5, [r4,#4]
 sub r5, #0x30
 str r5, [r4,#4]
@@ -19,5 +19,13 @@ ldr r4,=0x808371c
 mov lr,r4
 .short 0xf800
 pop {r4,r5}
-pop {r0}
-bx r0
+
+@Rerun the original routine that will break.
+mov r2, r9
+ldr r0, [r2, #0x0] @ pointer:03004E50 (Pointer to the work memory of the operation character)
+
+
+
+ldr r1,=0x08032440+1
+bx  r1
+
