15af68ffa17150c98006d88542354a1ab7d12abf
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2022-09-13 17:21:34 -0400

    Commit

--- Patches/ASMCs/ShootArrow/ShootArrow.asm
+++ Patches/ASMCs/ShootArrow/ShootArrow.asm
@@ -5,6 +5,7 @@
   .short 0xf800
 .endm
 
+.equ RefreshFogAndUnitMaps, 0x801A1F4 
 .equ AddArrowTrapTargetsToTargetList, 0x802E710 @r0 = x, r1 = y, r2 = trap type (7 for arrow) 
 .equ BWL_AddLossFromTrap, 0x802EA1C 
 .equ GenerateDisplayedTrapDamageTargets, 0x802E8A8 
@@ -18,13 +19,23 @@
 .equ ProcStartBlocking, 0x8002CE0 
 
 push {r4-r7, lr} 
-mov r4, r0 @ Parent proc 
+@mov r4, r0 @ Parent proc 
+
+blh RefreshFogAndUnitMaps
+
+mov r0, #0 
+mov r1, #0 
+blh InitTargets 
 
 ldr r3, =MemorySlot 
+ldrh r5, [r3, #4*1] @ damage 
 add r3, #0x0B*4 
+
 ldrh r6, [r3] @ XX 
-ldrh r7, [r3, #2] @ YY 
-mov r2, #7 @ arrow trap ID 
+ldrh r7, [r3, #2] @ YY
+mov r0, r6 
+mov r1, r7  
+mov r2, r5 @ damage 
 blh AddArrowTrapTargetsToTargetList 
 blh BWL_AddLossFromTrap 
 @blh GenerateDisplayedTrapDamageTargets 
@@ -36,11 +47,11 @@ blh InitTargets
 mov r0, r6 @ XX 
 mov r1, r7 @ YY 
 mov r2, #0 
-mov r3, #7 @ arrow trap ID 
-blh AddTarget, r5  
+mov r3, #7 @ arrow trap ID @ $36384 has a whole bunch of if statements related to target's r3 value 
+blh AddTarget, r4  
 mov r0, r6 @ XX 
 mov r1, r7 @ YY 
-mov r2, #0x7 @ arrow ID 
+mov r2, r5 @ damage 
 blh AddArrowTrapTargetsToTargetList 
 
 @ldr r0, =gProc_ShowArrowAnimation 
