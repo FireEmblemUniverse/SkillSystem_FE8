cead83a6b93d76726349a162407dced6e47f26ab
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2022-11-01 06:00:46 -0400

    Update AoE_PokemblemImmuneTargets.s

--- Patches/AoE/AoE_PokemblemImmuneTargets.s
+++ Patches/AoE/AoE_PokemblemImmuneTargets.s
@@ -1,10 +1,10 @@
 
-@ unit IDs 0xD0 - 0xEF are not damaged by aoe
+@ unit IDs 0xD1 - 0xEF are not damaged by aoe
 .thumb 
 mov r3, r0 @ target 
 ldr r0, [r3] 
 ldrb r0, [r0, #4] @ Unit ID 
-cmp r0, #0xD0 
+cmp r0, #0xD1
 blt RetFalse
 cmp r0, #0xF0
 bge RetFalse
