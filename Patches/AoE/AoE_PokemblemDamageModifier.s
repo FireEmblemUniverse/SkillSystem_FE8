0bae5214e9c8655f09bb9bfac4f588f000ff1814
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2022-11-18 09:48:57 -0500

    Update AoE_PokemblemDamageModifier.s

--- Patches/AoE/AoE_PokemblemDamageModifier.s
+++ Patches/AoE/AoE_PokemblemDamageModifier.s
@@ -49,6 +49,40 @@ b Neutral
 Neutral:
 @ modify to be 3/4 of damage 
 @lsr r1, r4, #2 @ 1/4 
+bl CheckDifficulty
+ldrb r1, [r5, #8] @ lvl 
+cmp r1, #50 
+ble NoCap 
+mov r1, #50 
+NoCap:
+cmp r0, #0 
+beq Easy 
+cmp r0, #1 
+beq Difficult 
+cmp r0, #2 
+beq Lunatic 
+b Easy 
+Difficult: 
+mov r0, r4 
+mul r0, r1 
+mov r1, #100 
+swi 6 
+lsr r0, #2 @ 87.5% @ 65% compared with normal attacks at worst (vs 75%)
+sub r4, r0 
+b Easy 
+Lunatic: 
+mov r0, r4 
+mul r0, r1 
+mov r1, #100 
+swi 6 
+mov r1, r0 
+lsl r1, #1 
+add r0, r1 @ 3x 
+lsr r0, #2 @ 1 - (50% * 75%) or 62.5% (47% at worst vs 50%)
+sub r4, r0 
+b Easy 
+
+Easy:
 mov r0, r4 
 @sub r0, r1 @ 3/4 
 pop {r4-r6}
