7b9d6f73435d874b8fccf94e974c60588f94b1aa
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2023-04-24 15:44:24 -0400

    Commit

--- EngineHacks/ExternalHacks/TrapRework/Modules/Pokemblem/asm/DisplayText.s
+++ EngineHacks/ExternalHacks/TrapRework/Modules/Pokemblem/asm/DisplayText.s
@@ -279,7 +279,17 @@ ldr r0,[r4]
 mov r1, r7 
 bl GetAdjacentTrapIndividual
 mov r4, r0  @&The DV
-b Usability_RetTrue 
+cmp r0,#0
+beq Usability_RetFalse
+ldr r4,=#0x3004E50
+@can't use if cantoing
+ldr r0,[r4]
+ldr r0,[r0,#0xC]
+mov r1,#0x40
+and r0,r1
+cmp r0,#0
+beq Usability_RetTrue
+b Usability_RetFalse
 
 
 
