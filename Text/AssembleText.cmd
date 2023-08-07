c2f4887e772aa7d3690c8ca7041781771c4f4ab3
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2023-08-07 15:42:14 -0400

    Commit

--- Text/AssembleText.cmd
+++ Text/AssembleText.cmd
@@ -2,7 +2,7 @@
 @echo off
 
 set "textprocess=%~dp0..\Tools\TextProcess\text-process-classic.exe"
-set "parsefile=%~dp0..\EventAssembler\Tools\ParseFile.exe"
+set "parsefile=%~dp0..\EventAssembler\Tools\ParseFileUTF8.exe"
 
   echo: | ("%textprocess%" text_buildfile.txt --parser-exe "%parsefile%" --installer "InstallTextData.event" --definitions "TextDefinitions.event")
 
