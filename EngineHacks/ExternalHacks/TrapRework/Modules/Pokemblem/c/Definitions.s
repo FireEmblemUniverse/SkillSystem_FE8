2b25d7adf293be6a47996fd87cfd09c045c3b070
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2024-02-24 07:55:55 -0500

    Fix signs

--- EngineHacks/ExternalHacks/TrapRework/Modules/Pokemblem/c/Definitions.s
+++ EngineHacks/ExternalHacks/TrapRework/Modules/Pokemblem/c/Definitions.s
@@ -2,7 +2,9 @@
 .include "C:/devkitPro/FE-CLib/reference/FE8U-20190316.s"
 SET_FUNC AddTrapExtFix, 0x802E2E1
 
-
+SET_FUNC SetFlag, 0x08083D81
+SET_FUNC ClearFlag, 0x08083D95
+SET_FUNC CheckFlag, 0x08083DA9
 
 
 
