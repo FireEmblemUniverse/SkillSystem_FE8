82599d1548fbd3d75ed402df948390e8a9970669
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2024-03-07 16:10:26 -0500

    Commit

--- Patches/Randomize/C_Code.c
+++ Patches/Randomize/C_Code.c
@@ -59,6 +59,8 @@ u16 HashShort_Ch(int number, int max){
 u32 HashSeed(u32 number) { 
   u32 hash = 5381;
   hash = ((hash << 5) + hash) ^ number;
+  hash = hash*2654435761 & ((2^31)-1);
+  hash = ((hash << 5) + hash) ^ number;
   //for (int i = 0; i < 9; ++i){
   //  if (TacticianName[i]==0) break;
   //  hash = ((hash << 5) + hash) ^ TacticianName[i];
@@ -181,6 +183,55 @@ int GetAverageCap(struct Unit* unit) {
 	//result += ClassMaxHPTable[classID].cap; 
 	return (result / i); 
 } 
+
+int* BubbleSort(int a[], int array_size)
+{
+    int i, j, temp;
+    for (i = 0; i < (array_size - 1); ++i)
+    {
+        for (j = 0; j < array_size - 1 - i; ++j )
+        {
+            if (a[j] > a[j+1])
+            {
+                temp = a[j+1];
+                a[j+1] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+	return a; 
+}
+
+int Get2ndLowestCap(struct Unit* unit) { 
+	int classID = unit->pClassData->number; 
+	int a[6]; 
+	a[0] = UNIT_POW_MAX(unit);
+	a[1] = UNIT_SKL_MAX(unit);
+	a[2] = UNIT_SPD_MAX(unit); 
+	a[3] = UNIT_DEF_MAX(unit); 
+	a[4] = UNIT_RES_MAX(unit);
+	a[5] = MagClassTable[classID].cap;
+	//a = ClassLuckTable[classID].growth; 
+	//a = ClassMaxHPTable[classID].growth; 
+	return BubbleSort(a, 6)[1]; 
+} 
+int Get2ndHighestCap(struct Unit* unit) { 
+	int classID = unit->pClassData->number; 
+	int a[6]; 
+	a[0] = UNIT_POW_MAX(unit);
+	a[1] = UNIT_SKL_MAX(unit);
+	a[2] = UNIT_SPD_MAX(unit); 
+	a[3] = UNIT_DEF_MAX(unit); 
+	a[4] = UNIT_RES_MAX(unit);
+	a[5] = MagClassTable[classID].cap;
+	//a = ClassLuckTable[classID].growth; 
+	//a = ClassMaxHPTable[classID].growth; 
+	return BubbleSort(a, 6)[4]; 
+} 
+
+
+
+
 int GetAverageGrowth(struct Unit* unit) { 
 	int classID = unit->pClassData->number; 
 	int i = 0; 
@@ -195,6 +246,8 @@ int GetAverageGrowth(struct Unit* unit) {
 	return (result / i); 
 } 
 
+
+
 // mov r2, r6 @ index in stat booster pointer of growth
 int GetRandomizedGrowth(struct Unit* unit, int growth, int id) { // index in stat booster pointer of growth
 	if (!CheckFlag(RandomizeGrowthsFlag_Link)) { return growth; } 
@@ -204,13 +257,15 @@ int GetRandomizedGrowth(struct Unit* unit, int growth, int id) { // index in sta
 	if ((uid >= 0x50) && (uid<0x87)) { growth += 10; } // so wilds match players 
 	if ((uid > 0x8C) && (uid<0xA0)) { growth += 10; } 
 	int newGrowth = growth; 
+	int avgGrowth = GetAverageGrowth(unit); 
 	
 	int cid = id + unit->pClassData->number;
-	newGrowth = HashByte_Global(newGrowth, (newGrowth/10) + avgCap, cid) + (avgCap / 3);
-	//if (abs(newGrowth - ((growth * 2) / 3)) < 15) { newGrowth = HashByte_Global(newGrowth, ((growth*2)/3) + 30, cid); } // reroll if too similar 
-	
+	newGrowth = HashByte_Global(newGrowth, avgGrowth + ((avgCap*3)/2), cid) + (avgCap/2);
+	int result = newGrowth; 
+	if ((result - growth) > 99) { result = 99; } // more than +99 growth looks bad on stat screen 
+	if ((growth - result) > 99) { result = growth - 99; } // more than +99 growth looks bad on stat screen 
 	// HP 
-	if (id == 10) { return newGrowth; } 
+	if (id == 10) { return result; } 
 	
 	// average random + half of the cap 
 	typedef int (*func_unit_int)(struct Unit*, int);
@@ -234,18 +289,26 @@ int GetRandomizedGrowth(struct Unit* unit, int growth, int id) { // index in sta
 	
 	int statCap = func(unit, 0);
 	
-	int result = (statCap*5)/2; //((statCap*3)/2); // + (newGrowth/5);
+	result = (statCap*5)/2; //((statCap*3)/2); // + (newGrowth/5);
 	newGrowth = HashByte_Global(statCap, avgCap/4, cid);
+	int max = (avgGrowth * 3)/2; 
+	if (result > max) { result = max; } 
+	
+
 	result -= newGrowth; 
+	if (((result - growth) < 20) && ((result - growth) >= 0)) { 
+		result += newGrowth; 
+	} 
+	if (((growth - result) < 20) && ((growth - result) >= 0)) { 
+		result -= newGrowth; 
+	} 
+	
+	
+	//if (abs(result - growth) < 20) { result = ((func(unit, result)*3)/2) + (result/5); } 
+	//if (abs(result - growth) < 20) { result = ((func(unit, result)*3)/2) + (result/5); } 
+	//if (abs(result - growth) < 20) { result = ((func(unit, result)*3)/2) + (result/5); } 
+	//if (abs(result - growth) < 20) { result = ((func(unit, result)*3)/2) + (result/5); } 
 	if (result < 0) { result = 0; } 
-	int max = (GetAverageGrowth(unit) * 3)/2; 
-	if (result > max) { newGrowth = max; } 
-	
-	//if (abs(result - growth) < 20) { result = ((func(unit, result)*3)/2) + (result/5); } 
-	//if (abs(result - growth) < 20) { result = ((func(unit, result)*3)/2) + (result/5); } 
-	//if (abs(result - growth) < 20) { result = ((func(unit, result)*3)/2) + (result/5); } 
-	//if (abs(result - growth) < 20) { result = ((func(unit, result)*3)/2) + (result/5); } 
-	
 	if ((result - growth) > 99) { result = 99; } // more than +99 growth looks bad on stat screen 
 	if ((growth - result) > 99) { result = growth - 99; } // more than +99 growth looks bad on stat screen 
 	return result; 
@@ -277,9 +340,16 @@ void RandomizeStats(struct Unit* unit) {
 
 int RandomizeStatCap(int statCap, struct Unit* unit, int variance) { 
 	int avgCap = GetAverageCap(unit); 
-	int min = (avgCap / 3); // eg. 10 - 20 
-	int max = ((avgCap * 3)/2) - min; // eg. 45 - 90 
-	if ((avgCap - statCap) > 10) { max = (max*3)/2; } 
+	int min = (avgCap / 4); // eg. 8 - 15
+	int max = avgCap; 
+	int low = Get2ndLowestCap(unit); 
+	int high = Get2ndHighestCap(unit); 
+	max -= min;
+	if (statCap >= high) { max = (max*13)/10; } // encourage best 2 stats to be lower  
+	if (statCap <= low) { max = (max*22)/10; } // encourage worst 2 stats to be higher 
+	if ((statCap > low) && (statCap < high)) { max = ((avgCap * 17)/10); } // eg. 45 - 90 
+	 
+	
 	int classID = unit->pClassData->number + variance; 
 	int result = HashByte_Global(statCap, max, classID) + min;
 	
@@ -288,12 +358,12 @@ int RandomizeStatCap(int statCap, struct Unit* unit, int variance) {
 	if (abs(result - statCap) < (avgCap/2)) { result = HashByte_Global(result, max, classID) + min; } // reroll if too similar 
 	if (abs(result - statCap) < (avgCap/2)) { result = HashByte_Global(result, max, classID) + min; } // reroll if too similar 
 	if (abs(result - statCap) < 16) { result = HashByte_Global(result, max, classID) + min; } // reroll if too similar 
-	if (abs(result - statCap) < 11) { result = HashByte_Global(result, max + min, classID); } // reroll if too similar 
-	if (abs(result - statCap) < 6) { result = HashByte_Global(result, max + min, classID); } // reroll if too similar 
+	if (abs(result - statCap) < 11) { result = HashByte_Global(result, max, classID) + min; } // reroll if too similar 
+	if (abs(result - statCap) < 6) { result = HashByte_Global(result, max + min, classID); } // reroll if too similar (no min) 
 	
 	
 	
-	if (result > 60) { result = 60; } // probably unnecessary 
+	if (result >= 60) { result = 60 - HashByte_Global(result, min, classID); } 
 	return result; 
 
 } 
@@ -544,3 +614,50 @@ void StartShopScreen(struct Unit* unit, u16* inventory, u8 shopType, ProcPtr par
 
     return;
 }
+
+
+
+/*
+int GetLowestGrowth(struct Unit* unit) { 
+	int classID = unit->pClassData->number; 
+	int a = 0; 
+	int result = unit->pClassData->growthPow + 10; 
+	if (a < result) { result = a; } 
+	a = unit->pClassData->growthSkl + 10; 
+	if (a < result) { result = a; } 
+	a = unit->pClassData->growthSpd + 10;
+	if (a < result) { result = a; } 
+	a = unit->pClassData->growthDef + 10; 
+	if (a < result) { result = a; } 
+	a = unit->pClassData->growthRes + 10; 
+	if (a < result) { result = a; } 
+	a = MagClassTable[classID].growth + 10; 
+	if (a < result) { result = a; } 
+	//a = ClassLuckTable[classID].growth; 
+	//a = ClassMaxHPTable[classID].growth; 
+	return result; 
+} 
+int GetHighestGrowth(struct Unit* unit) { 
+	int classID = unit->pClassData->number; 
+	int a = 0; 
+	int result = unit->pClassData->growthPow + 10; 
+	if (a > result) { result = a; } 
+	a = unit->pClassData->growthSkl + 10; 
+	if (a > result) { result = a; } 
+	a = unit->pClassData->growthSpd + 10;
+	if (a > result) { result = a; } 
+	a = unit->pClassData->growthDef + 10; 
+	if (a > result) { result = a; } 
+	a = unit->pClassData->growthRes + 10; 
+	if (a > result) { result = a; } 
+	a = MagClassTable[classID].growth + 10; 
+	if (a > result) { result = a; } 
+	//a = ClassLuckTable[classID].growth; 
+	//a = ClassMaxHPTable[classID].growth; 
+	return result; 
+} */
+
+
+
+
+
