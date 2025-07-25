870444674b68568272b333ea4c72a70664451847
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2025-07-25 11:31:11 -0400

    fix mirrored traps

--- Patches/MirrorMapSprites/Hooks.asm
+++ Patches/MirrorMapSprites/Hooks.asm
@@ -22,6 +22,22 @@ pop {r0}
 bx r0 
 .ltorg 
 
+.global InitSpriteHook
+.type InitSpriteHook, %function 
+InitSpriteHook: 
+push {lr} 
+ldr r0, [r3] 
+str r1, [r0] 
+str r0, [r2] 
+mov r1, r0 
+add r1, #0xC 
+str r1, [r3] 
+
+mov r3, #0 
+strb r3, [r0, #0xA] 
+pop {r3} 
+bx r3 
+.ltorg 
 
 
 
