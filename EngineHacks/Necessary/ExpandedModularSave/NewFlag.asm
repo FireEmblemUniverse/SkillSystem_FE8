c30c6f9cfad56265051fac96f64ea7b86305e16b
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2025-07-27 11:26:00 -0400

    hook set/unset/check flag

--- EngineHacks/Necessary/ExpandedModularSave/NewFlag.asm
+++ EngineHacks/Necessary/ExpandedModularSave/NewFlag.asm
@@ -11,6 +11,108 @@
 	.thumb
 	.equ NewFlagsRam, 0x203F548 @ [0x203F548..0x203F648]!!?
 
+.equ LastVanillaFlag, 0xC8
+
+.global SetGlobalFlag_Hook
+.type SetGlobalFlag_Hook, %function 
+SetGlobalFlag_Hook: 
+push {lr} 
+
+@ldr r1, =0x12C @ Last vanilla flag  
+mov r1, #LastVanillaFlag @ Last vanilla global flag  
+cmp r3, r1
+blt VanillaSetGlobalFlag 
+
+mov r0, r3 
+sub r0, r1 
+ldr r1, =EventFlags
+lsl r1, #3 @8 flags per byte 
+add r0, r1 
+bl SetNewFlag_No_sC
+b ExitSetGlobalFlag_Hook
+VanillaSetGlobalFlag: 
+sub r0, r3, r0 
+ldr r1, =0x8083CD4 
+ldr r1, [r1] @ gEventIdMaskLookup
+add r0, r1 
+ldrb r1, [r2] 
+ldrb r0, [r0] 
+orr r1, r0 
+strb r1, [r2] 
+
+ExitSetGlobalFlag_Hook: 
+pop {r3} 
+bx r3 
+.ltorg 
+
+
+.global UnsetGlobalFlag_Hook
+.type UnsetGlobalFlag_Hook, %function 
+UnsetGlobalFlag_Hook: 
+push {lr} 
+
+@ldr r1, =0x12C @ Last vanilla flag  
+mov r3, #LastVanillaFlag @ Last vanilla global flag  
+cmp r2, r3
+blt VanillaUnsetGlobalFlag 
+
+mov r0, r2
+sub r0, r3 
+ldr r1, =EventFlags
+lsl r1, #3 @8 flags per byte 
+add r0, r1 
+bl UnsetNewFlag_No_sC
+b ExitUnsetGlobalFlag_Hook
+VanillaUnsetGlobalFlag: 
+lsl r0, #24 
+lsr r3, r0, #24 
+ldr r0, =0x8083D14
+ldr r0, [r0] @ gEventIdMaskLookup
+add r1, r0
+ldrb r0, [r1] 
+and r0, r3 
+strb r0, [r1] 
+
+ExitUnsetGlobalFlag_Hook: 
+pop {r3} 
+bx r3 
+.ltorg 
+
+.global CheckGlobalFlag_Hook
+.type CheckGlobalFlag_Hook, %function 
+CheckGlobalFlag_Hook: 
+push {lr} 
+
+@ldr r1, =0x12C @ Last vanilla flag  
+mov r1, #LastVanillaFlag @ Last vanilla global flag 
+cmp r3, r1
+blt VanillaCheckGlobalFlag 
+
+mov r0, r3
+sub r0, r1 
+ldr r1, =EventFlags
+lsl r1, #3 @8 flags per byte 
+add r0, r1 
+bl CheckNewFlag_No_sC
+b ExitCheckGlobalFlag_Hook
+VanillaCheckGlobalFlag: 
+ldr r1, =0x8083D60
+ldr r1, [r1] @ gEventIdMaskLookup
+add r0, r1 
+ldrb r1, [r2] 
+ldrb r0, [r0] 
+and r0, r1 
+cmp r0, #0 
+beq ExitCheckGlobalFlag_Hook
+mov r0, #1
+ExitCheckGlobalFlag_Hook: 
+pop {r3} 
+bx r3 
+.ltorg 
+
+
+
+
 @ parameters: 
 @ given r0 as Short of which flag, we will Set, Unset, or Check it. 
 @ check returns boolean in r0 of 1 = true, 0 = false 
